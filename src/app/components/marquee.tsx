eec9a11e538ab0ae908bcb39706c2bc32ea77ef1
Pemo0001 <pemo0001@stud.kea.dk>
2025-04-25 12:56:12 +0100

    Marquue speed fix

--- src/app/components/marquee.tsx
+++ src/app/components/marquee.tsx
@@ -7,7 +7,7 @@ interface MarqueeProps {
 
 export default function MarqueeComponent({ text, direction = "left" }: MarqueeProps) {
   return (
-    <Marquee speed={40} pauseOnHover={true} direction={direction} className="bg-black text-white h-28 font-extrabold font-exposure text-8xl">
+    <Marquee speed={100} pauseOnHover={true} direction={direction} className="bg-[#000000] text-white h-28 font-extrabold font-exposure text-8xl">
       <p>{text}</p>
       <img src="images/sydforsolenlogo_white.png" alt="logo" className="h-24 mx-8 w-auto object-contain" />
       <p>{text}</p>
