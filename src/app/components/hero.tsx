abe36f60729da9604e479784297c03383e7ffbbb
Nikolaj <Nikolajgottschalck@hotmail.com>
2025-05-23 11:37:20 +0200

    Enhance Hero component layout with responsive video backgrounds and improved clipping for better visual presentation

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,8 +1,15 @@
 export default function Hero() {
   return (
+    <div
+      className="relative h-[100vh]"
+      style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
+    >
+      <div className="fixed bottom-0 h-[100vh] w-full">
     <div className="h-screen w-auto overflow-hidden">
         <video src="/images/sydforsolenvideo.mp4" autoPlay muted loop className="w-full h-full object-cover md:block hidden" />
         <video src="/images/sydforsolenvideomobile.mp4" autoPlay muted loop className="w-full h-full object-cover md:hidden" />
+        </div>
+      </div>
     </div>
   );
 }
